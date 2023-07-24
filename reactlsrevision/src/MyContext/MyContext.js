9cbdf7e0f3abdcf4ab626c2da9132f2836345d7f
ManojNadar <manojviru03@gmail.com>
2023-07-24 11:13:27 +0530

    Update MyContext.js

--- reactlsrevision/src/MyContext/MyContext.js
+++ reactlsrevision/src/MyContext/MyContext.js
@@ -9,7 +9,6 @@ const reducer = (state, action) => {};
 const MyContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-
   return (
     <>
       <MyContext.Provider value={{ state }}>{children}</MyContext.Provider>
